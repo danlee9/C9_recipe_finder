3a00df7fb1ca278fe6755aa7df2bcb1233238abb
kyle kenney <kairukenney@gmail.com>
2016-08-30 13:06:01 -0700

    working routing but home link needs work

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ app.config(function($routeProvider){
         .when('/',{
             templateUrl: "index.html"
         })
+        .when('/recipe-list',{
+            templateUrl: "list.html"
+        })
+        .when('/recipe',{
+            templateUrl: "recipe.html"
+        })
+        .otherwise({
+            redirectTo: "/"
+        })
 });
 app.controller('mainController', function($log){
     $log.info("I am ready to load!");
